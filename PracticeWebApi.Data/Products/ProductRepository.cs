2f2d8c0e0736be8899926f2a444ccba5022c6dbd
jamesjr19 <ruwartja@gmail.com>
2020-06-10 20:10:09 -0400

    Added Product Repo SQL connections

--- PracticeWebApi.Data/Products/ProductRepository.cs
+++ PracticeWebApi.Data/Products/ProductRepository.cs
@@ -8,6 +8,23 @@ namespace PracticeWebApi.Data.Products
 {
     public class ProductRepository : IProductRepository
     {
+        private string _connectionString = @"Data Source = MSI\SQLEXPRESS; Initial Catalog = PracticeCommerce; Integrated Security = True;";
+        private string _addProduct =
+            @"INSERT INTO Products (id, name, description, price, groupId, isActive) VALUES " +
+            "(@Id, @Name, @Description, @Price, @GroupId, @IsActive)";
+        private string _activateProduct = "SELECT * FROM Products WHERE [Id] = @ProductId";
+        private string _deactiveProduct = "SELECT * FROM Products WHERE [Id] = @ProductId";
+        private string _findProductById = "DELETE FROM Products WHERE [Id] = @ProductId";
+        private string _getProductsByGroupId = "SELECT * FROM Products WHERE [Id] = @ProductId";
+        private string _updateProduct = @"  
+            UPDATE Products
+              SET [name] = @Name,
+	              [description] = @Description,
+	              [price] = @Price,
+	              [groupId] = @GroupId,
+	              [isActive] = @IsActive,
+            WHERE [Id] = @Id";
+
         private IList<ProductDataEntity> _Products;
 
         public ProductRepository()
@@ -28,27 +45,45 @@ namespace PracticeWebApi.Data.Products
 
         public Task<ProductDataEntity> AddProduct(ProductDataEntity product)
         {
-            throw new NotImplementedException();
+            _Products.Add(product);
+
+            return Task.FromResult(product);
         }
 
         public Task DeactiveProduct(string productId)
         {
-            throw new NotImplementedException();
+            var product = _Products.Where(p => p.Id == productId).FirstOrDefault();
+
+            if (product is null) throw new ResourceNotFoundException($"Product Id {productId} does not exist. Please try again.");
+
+            product.IsActive = false;
+
+            return Task.CompletedTask;
         }
 
         public Task<ProductDataEntity> FindProductById(string productId)
         {
-            throw new NotImplementedException();
+            var product = _Products.Where(p => p.Id == productId).FirstOrDefault();
+
+            if (product is null) throw new ResourceNotFoundException($"Product Id {productId} does not exist. Please try again.");
+
+            return Task.FromResult(product);
         }
 
         public Task<IList<ProductDataEntity>> GetProductsByGroupId(string groupId)
         {
-            throw new NotImplementedException();
+            IList<ProductDataEntity> products = _Products.Where(p => p.GroupId == groupId && p.IsActive).ToList();
+
+            return Task.FromResult(products);        
         }
 
         public Task UpdateProduct(ProductDataEntity product)
         {
-            throw new NotImplementedException();
+            _Products= _Products.Where(p => p.Id != product.Id).ToList();
+
+            _Products.Add(product);
+
+            return Task.CompletedTask;
         }
     }
 }
